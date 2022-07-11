c44c5b34e95085d685181b51a45364af7867162e
Kristina <k.nejgum.kn@gmail.com>
2022-07-11 15:30:12 +0200

    2b.Parametrisierung + Datenübergabe von AUSSEN

--- script_01.js
+++ script_01.js
@@ -2,3 +2,36 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+function test() 
+{
+    console.log("Hallo Robert!");
+}
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeNamen();
+// console.log(firstName); // Fehler: Scope!
+
+function ausgabeNamen() {
+    let firstName = "Anna"; // what happens in Vegas, ...
+    console.log("Hallo " + firstName + "!");
+}
+
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamenParam("Anna"); // Argument 
+ausgabeNamenParam("Craig");
+ausgabeNamenParam("Fatih");
+
+function ausgabeNamenParam(firstName) { // Parameter
+    console.log("Hallo " + firstName + "!");
+}
